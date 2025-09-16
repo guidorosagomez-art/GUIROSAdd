b5ccdc129caa51176d590f792dc3d430a313f9f9
guidorosagomez-art <guidorosagomez@gmail.com>
2025-09-16 16:22:40 -0300

    Add files via upload

---
+++ data.js
@@ -0,0 +1,34 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-360_f_322882600_y6jbonld7yldrru5lfqreuq8yuwasfgg-1",
+      "name": "360_F_322882600_y6JbONLD7YLdRrU5LFQReuq8YUwasfgg (1)",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        }
+      ],
+      "faceSize": 270,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "qtvr",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
