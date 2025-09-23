ef30597fd8c66c6c4610f40ca2911429f23acfcb
guidorosagomez-art <guidorosagomez@gmail.com>
2025-09-23 16:33:24 -0300

    Add files via upload

--- data.js
+++ data.js
@@ -1,8 +1,8 @@
 var APP_DATA = {
   "scenes": [
     {
-      "id": "0-ella-seq-iejad",
-      "name": "ELLA SEQ IEJAD",
+      "id": "0-ewweew",
+      "name": "ewweew",
       "levels": [
         {
           "tileSize": 256,
@@ -21,7 +21,44 @@ var APP_DATA = {
         "fov": 1.5707963267948966
       },
       "linkHotspots": [],
-      "infoHotspots": []
+      "infoHotspots": [
+        {
+          "yaw": -0.19986275330989045,
+          "pitch": 0.1689306987482233,
+          "title": "Title",
+          "text": "ESPACIO POLIVALENTE<div><br></div>"
+        }
+      ]
+    },
+    {
+      "id": "1-eeeeeeww",
+      "name": "eeeeeeww",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        }
+      ],
+      "faceSize": 450,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": [
+        {
+          "yaw": -0.6626791011364865,
+          "pitch": -0.14608152765874038,
+          "title": "Title",
+          "text": "ESCALERA<div><br></div>"
+        }
+      ]
     }
   ],
   "name": "Project Title",
